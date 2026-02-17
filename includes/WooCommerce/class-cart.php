00103e3dd70ba795ab11702df8b4f0f8819b460e
Shawn Mosher <10968229+TheAggressive@users.noreply.github.com>
2026-02-16 18:48:40 -0700

    fix: remove unused cart script enqueue function
    - Eliminated the `enqueue_cart_script` method from the Cart class, as it was not being utilized, streamlining the codebase.

--- includes/WooCommerce/class-cart.php
+++ includes/WooCommerce/class-cart.php
@@ -31,7 +31,6 @@ class Cart {
 	 */
 	public function init() {
 		add_filter( 'woocommerce_add_to_cart_fragments', array( $this, 'cart_fragments' ), 10, 1 );
-		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_cart_script' ), 20 );
 	}
 
 	/**
@@ -51,31 +50,5 @@ class Cart {
 		$fragments['span.aggressive-apparel-cart__count'] = '<span class="aggressive-apparel-cart aggressive-apparel-cart__count">' . esc_html( (string) $cart_count ) . '</span>';
 
 		return $fragments;
-	}
-
-	/**
-	 * Enqueue cart script
-	 *
-	 * @return void
-	 */
-	public function enqueue_cart_script() {
-		if ( ! is_cart() && ! is_checkout() ) {
-			return;
-		}
-
-		$theme_uri = get_template_directory_uri();
-		$theme     = wp_get_theme();
-		$version   = ! $theme->errors() ? $theme->get( 'Version' ) : '1.0.0';
-
-		wp_enqueue_script(
-			'aggressive-apparel-cart',
-			$theme_uri . '/build/scripts/cart.js',
-			array( 'jquery' ),
-			$version,
-			array(
-				'in_footer' => true,
-				'strategy'  => 'defer',
-			)
-		);
 	}
 }
